76312bbbfaca034063c4e8f322cc848b692cdef3
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-23 01:00:50 +0400

    update

--- src/components/Chat/ChatHeader/ChatHeader.jsx
+++ src/components/Chat/ChatHeader/ChatHeader.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from "react-redux";
 
 export const ChatHeader = () => {
-  const userData = useSelector((state) => state.user.name);
+  const roomData = useSelector((state) => state.chat.roomId);
   return (
     <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
       <div className="w-1/2 flex items-center">
         <div className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center">
-        {userData?.at(0)}
+        {roomData?.at(0)}
         </div>
-        <h3 className="font-sans text-md px-2">{userData}</h3>
+        <h3 className="font-sans text-md px-2">{roomData}</h3>
       </div>
     </div>
   );
