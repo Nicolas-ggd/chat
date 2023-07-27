e2143502e4c89642f36715647c04d7592d1d6214
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-28 01:25:59 +0400

    add click outside function && display searched user with message button

--- src/components/Chat/ChatFriends/ChatFriendsModal.jsx
+++ src/components/Chat/ChatFriends/ChatFriendsModal.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
 import ChatIcon from "@mui/icons-material/Chat";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 
 export const ChatFriendsModal = ({ toggleModal }) => {
   const [isValue, setIsValue] = useState("");
   const [isSearch, setIsSearch] = useState([]);
+  const [isShown, setIsShown] = useState(true);
   const userId = useSelector((state) => state.user.userId);
+  const outsideRef = useRef();
 
   const handleFriendsSearch = (e) => {
     setIsValue(e.target.value);
@@ -34,84 +37,97 @@ export const ChatFriendsModal = ({ toggleModal }) => {
     searchFriends();
   }, [isValue, userId]);
 
-  return (
-    <div
-      className="w-full h-screen absolute bg-gray-100 animate-fade animate-once animate-duration-1000 animate-ease-in"
-      style={{ backgroundColor: "rgba(12, 11, 11, 0.92)", zIndex: "9999" }}
-    >
-      <div
-        aria-hidden="true"
-        className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
-      >
-        <div className="m-auto mt-auto flex w-screen items-center h-full relative max-w-md max-h-full">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button
-              type="button"
-              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="authentication-modal"
-              onClick={toggleModal}
-            >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-            <div className="px-6 py-6 lg:px-8" style={{ width: "500px" }}>
-              <h3 className="mb-4 text-md font-medium text-gray-900 dark:text-white">
-                Invite friends to 's room
-              </h3>
-              <form className="space-y-6" action="#">
-                <div className="relative flex items-center">
-                  <input
-                    type="text"
-                    name="text"
-                    id="text"
-                    className="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white py-5 text-xl"
-                    placeholder="Search friends"
-                    value={isValue}
-                    onChange={handleFriendsSearch}
-                  />
-                </div>
-              </form>
+  const onClickOutside = () => {
+    setIsShown(false);
+    toggleModal();
+  };
 
-              <div className="flex flex-col w-100">
-                <h2 className="text-md dark:text-white py-2">
-                  Previous Conversations
-                </h2>
-                {isSearch &&
-                  isSearch?.length > 0 &&
-                  isSearch?.map((item, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="w-full p-3 bg-gray-200 dark:bg-gray-700 rounded-md hover:dark:bg-gray-600 cursor-pointer flex justify-between"
-                      >
-                        <p className="text-dark dark:text-white">
-                          # {item?.name}
-                        </p>
-                        <div>
-                          <ChatIcon className="text-white" />
-                        </div>
-                      </div>
-                    );
-                  })}
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (outsideRef.current && !outsideRef.current.contains(event.target)) {
+        onClickOutside && onClickOutside();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [onClickOutside]);
+
+  const clickConversation = (item) => {
+    console.log(item, "item");
+  };
+
+  return (
+    <>
+      {isShown && (
+        <div
+          className="w-full h-screen absolute bg-gray-100 animate-fade animate-once animate-duration-1000 animate-ease-in"
+          style={{ backgroundColor: "rgba(12, 11, 11, 0.92)", zIndex: "9999" }}
+        >
+          <div
+            aria-hidden="true"
+            className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          >
+            <div className="m-auto mt-auto flex w-screen items-center h-full relative max-w-md max-h-full">
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div
+                  className="px-6 py-6 lg:px-8"
+                  style={{ width: "500px" }}
+                  ref={outsideRef}
+                >
+                  <form className="space-y-6" action="#">
+                    <div className="relative flex items-center">
+                      <input
+                        type="text"
+                        name="text"
+                        id="text"
+                        className="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white py-5 text-xl"
+                        placeholder="You can search friends with their name"
+                        value={isValue}
+                        onChange={handleFriendsSearch}
+                      />
+                    </div>
+                  </form>
+
+                  <div className="flex flex-col w-100">
+                    <h2 className="text-sm dark:text-white py-2">
+                      Previous Conversations
+                    </h2>
+                    {isSearch &&
+                      isSearch?.length > 0 &&
+                      isSearch?.map((item, index) => {
+                        return (
+                          <div
+                            key={index}
+                            className="w-full p-3 bg-gray-200 dark:bg-gray-700 rounded-md hover:dark:bg-gray-600 cursor-pointer flex items-center justify-between transition duration-200"
+                          >
+                            <div className="flex items-center">
+                              <div className="bg-green-400 h-8 w-8 rounded-full flex items-center justify-center px-1">
+                                <SmartToyIcon />
+                              </div>
+                              <p className="text-dark dark:text-white px-2">
+                                # {item?.name}
+                              </p>
+                            </div>
+                            <div className="hover:dark:bg-green-600 p-1 rounded-md transition duration-200" onClick={() => clickConversation(item)}>
+                              <button className="px-2 text-dark dark:text-white">
+                                <ChatIcon className="text-white mx-1" />
+                              Message
+                              </button>
+                            </div>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
